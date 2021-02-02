9820b57e17501f6e9c61a574c199eae44f26054a
Michal Szczepanik <micszczepanik@gmail.com>
2021-02-02 20:01:51 +0100

    updating initial Values

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const { ApolloServer } = require('apollo-server')
+const { ApolloServer, gql } = require('apollo-server')
 
-const typeDefs = `
+const typeDefs = gql`
     type Query {
-        greetings: String
+        firstName: String,
+        email: String,
+        pets: [String]
     }
 `;
 
-const server = new ApolloServer({ typeDefs })
+const data = {
+    firstName: 'Yogi',
+    email: 'yogi@ncdc.pl',
+    pets: ['Dog, Cat, Bat']
+}
+
+const server = new ApolloServer({ typeDefs, rootValue: data })
 
 server.listen({port: 4000}).then(res => res.url)
