4591c58c2b60dd9639b34ee989023c3ab15a04ec
Michal Szczepanik <micszczepanik@gmail.com>
2021-02-02 19:41:33 +0100

    configure and run server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Yogii')
+const { ApolloServer } = require('apollo-server')
+
+const typeDefs = `
+    type Query {
+        greetings: String
+    }
+`;
+
+const server = new ApolloServer({ typeDefs })
+
+server.listen({port: 4000}).then(res => res.url)
